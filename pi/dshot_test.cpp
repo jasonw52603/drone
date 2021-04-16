b71f3c9ccbe2e0afc4f980e98e9817501fc497e6
Jason <jasontzewong@gmail.com>
2021-04-15 22:41:27 -0400

    deciphered right/left shift and bitwise or

--- pi/dshot_test.cpp
+++ pi/dshot_test.cpp
@@ -1,13 +1,19 @@
 #include "stdint.h"
 #include <stdio.h>
+#include <iostream>
 using namespace std;
 
 int main() {
-	int testet = 2;
-	printf("this");
+	uint16_t testet = 11;
+    uint16_t testettwo = 4;
+    uint16_t result;
 
-
-	printf("%f", testet);
+    // cout << std::hex << testet;
+    //testet >>= 4;
+    // cout << std::hex << testet;
+    result = testet|testettwo;
+    cout << std::hex << testet << "|" << testettwo << ":" << result;
+    
 
 	return 0;
 }
